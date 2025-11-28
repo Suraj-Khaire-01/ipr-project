6f57470a10486a7e84c74ae7747c61dcff3e8aee
Suyash_Kakade <kakadesuyash1@gmail.com>
2025-11-28 23:36:21 +0530

    backend url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: env.VITE_BACKEND_URL,
+        target: ' https://ipr-project.onrender.com/api',
         changeOrigin: true,
         secure: false,
       }
