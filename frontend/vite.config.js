e0e951911762d4e6362873f962a5082dd1c3c2de
Suyash_Kakade <kakadesuyash1@gmail.com>
2025-11-28 23:37:16 +0530

    backend url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-const backend_url = import.meta.env.VITE_BACKEND_URL;
+// const backend_url = import.meta.env.VITE_BACKEND_URL;
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
