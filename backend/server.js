012c671760c1316dec39f986481a6e3f4b26bcd3
Suyash_Kakade <kakadesuyash1@gmail.com>
2025-11-29 01:42:48 +0530

    origin

--- backend/server.js
+++ backend/server.js
@@ -68,6 +68,8 @@ app.use(cors({
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5173',
     'https://ipr-project-chi.vercel.app',
+    "https://ipr-project-chi.vercel.app/",
+  /\.vercel\.app$/
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
